7ebb6482be169bc8cc5dbba8f3c18cad65b355f5
TD0961 <tensaedeme61@gmail.com>
2025-04-09 07:24:26 -0700

    Initial commit of Injera Gebeya project

---
+++ Client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./public/**/*.html",
+    "./index.html", ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
