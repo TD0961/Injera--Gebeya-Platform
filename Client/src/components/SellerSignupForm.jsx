a42f97ea1a147edca101ad0238c9d0eb1f206956
TD0961 <tensaedeme61@gmail.com>
2025-04-26 09:16:51 -0700

    initiating a seller signup page

---
+++ Client/src/components/SellerSignupForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SellerSignupForm() {
+    return (
+        <div>
+            <h1>Seller Signup Form</h1>
+        </div>
+    );
+}
+
+export default SellerSignupForm;
