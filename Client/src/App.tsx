eb773ad25112198fd1c6a4cc15c2405128afa8d3
TD0961 <tensaedeme61@gmail.com>
2025-10-02 17:41:19 +0300

    re configureing with typescript

---
+++ Client/src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+    return (
+        <div className="flex h-screen justify-center items-center">
+            <div className="bg-blue-500 p-8 text-white">
+                This div is centered.
+            </div>
+        </div>
+    );
+};
+export default App;
